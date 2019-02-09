29ae755998cc3cb5123ac3056327c48a7ed63441
ray755 <apriyandim0@gmail.com>
2019-02-09 09:31:50 +0700

    tugaspy1

---
+++ program1.py
@@ -0,0 +1,16 @@
+a= input ("masukan a : ")
+b= input ("masukan b : ")
+c= input ("masukan c : ")     
+if a>b: 
+    if a>c:
+        print ("Nilai Terbesarnya adalah",a)
+    else :
+        print ("Nilai Terbesarnya adalah",c)
+elif b>c:
+    print ("Nilai Terbesarnya adalah",b)
+else :
+    print ("Nilai Terbesarnya adalah",c)
+
+    
+
+
